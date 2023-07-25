f145b3831491ff9932857b70c976ffa40a9ce22b
Daniil Lashin <danil-lashin@yandex.ru>
2023-07-25 11:16:59 +0300

    Address converter

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,103 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <section class="section">
+    <div class="container">
+      <h1 class="is-1 title">Tenet Converter</h1>
+      <div class="columns">
+        <div class="column">
+          <div class="box">
+            <div class="field">
+              <label class="label">Address</label>
+              <div class="control">
+                <input class="input" v-model="addressInput" type="text" placeholder="tenet1px8tpk4f8t6zxyv8v80ghdle4ujc6jveetl9vd">
+              </div>
+
+              <ul v-if="address" class="list">
+                <li><b>Address (EIP-55)</b> {{ address.eip55 }}</li>
+                <li><b>Bech32 Acc</b> {{ address.acc }}</li>
+                <li><b>Bech32 Val</b> {{ address.val }}</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          Second column
+        </div>
+        <div class="column">
+          Third column
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import {bech32} from "bech32";
+import {ref, watch} from "vue";
+import * as eip55 from "eip55";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+bech32.decode('tenet1px8tpk4f8t6zxyv8v80ghdle4ujc6jveetl9vd')
+
+let addressInput = ref("");
+let address = ref(null);
+
+watch(addressInput, (val) => {
+    try {
+      let words = [];
+
+      if (val.startsWith("0x") && val.length === 42 && eip55.verify(val, true)) {
+        words = bech32.toWords(hexToBytes(val.substr(2)));
+      } else {
+        words = bech32.decode(val).words;
+      }
+
+      address.value = {
+        eip55: eip55.encode("0x" + bytesToHex(bech32.fromWords(words))),
+        acc: bech32.encode('tenet', words),
+        val: bech32.encode('tenetval', words),
+      }
+    } catch(e) {
+      console.log(e);
+      address.value = null;
+    }
+})
+
+// Convert a hex string to a byte array
+function hexToBytes(hex) {
+  let bytes = [];
+  for (let c = 0; c < hex.length; c += 2)
+    bytes.push(parseInt(hex.substr(c, 2), 16));
+  return bytes;
 }
+
+// Convert a byte array to a hex string
+function bytesToHex(bytes) {
+  let hex = [];
+  for (let i = 0; i < bytes.length; i++) {
+    let current = bytes[i] < 0 ? bytes[i] + 256 : bytes[i];
+    hex.push((current >>> 4).toString(16));
+    hex.push((current & 0xF).toString(16));
+  }
+  return hex.join("");
+}
+
+// tenet1px8tpk4f8t6zxyv8v80ghdle4ujc6jveetl9vd
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.list {
+  white-space: nowrap;
+  overflow: scroll;
+  max-width: 100%;
+
+  margin-top: 10px;
+}
+
+.list b {
+  display: block;
+}
+
+.list li {
+  margin-top: 10px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import 'bulma/bulma.sass'
+
+import { Buffer } from 'buffer';
+
+// @ts-ignore
+window.Buffer = Buffer;
+
 createApp(App).mount('#app')
